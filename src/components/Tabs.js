13ede8f1553bd26ca512640853216e036837c396
diptendu <mandaldiptendu99@gmail.com>
2023-08-17 12:55:28 +0530

    First Commit

---
+++ src/components/Tabs.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+
+const Tabs = ({tabsData}) => {
+
+    let [tab1, tab2, tab3] = tabsData;
+
+    let [showContent, setShowConent] = useState("");
+
+    return (
+        <div>
+            <ul>
+                <li onClick={() => setShowConent(tab1.content)}>{tab1.title}</li>
+                <li onClick={() => setShowConent(tab2.content)}>{tab2.title}</li>
+                <li onClick={() => setShowConent(tab3.content)}>{tab3.title}</li>
+            </ul>
+            <div>{showContent}</div>
+        </div>
+    )
+}
+
+
+export default Tabs;
